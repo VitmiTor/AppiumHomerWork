2bd4663263660da918256615e13328488f2a233d
VitmiTor <torremvictor@gmail.com>
2023-02-21 20:29:58 -0500

    firstcomit

---
+++ src/test/java/firstTest.java
@@ -0,0 +1,17 @@
+import base.BaseTest;
+import io.appium.java_client.android.AndroidDriver;
+import org.testng.annotations.Test;
+
+public class firstTest extends BaseTest {
+
+
+    @Test
+    public void test1() {
+        
+    }
+
+    @Override
+    public void initPages(AndroidDriver driver) {
+
+    }
+}
